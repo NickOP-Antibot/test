aed0f2b5752a38038ae4e6c41b886f1e78effbdf
NickOP-Antibot <chitsanupat2549@gmail.com>
2026-02-04 18:12:52 +0700

    Refactor main function to improve user input handling and enhance name search functionality

--- testing.cpp
+++ testing.cpp
@@ -4,16 +4,74 @@
 
 using namespace std;
 
+void checkname(string, bool &);
+
 int main() {
+    string name;
+    bool found = false;
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    checkname(name, found);
+
+    if (found == false) {
+        cout << "\n Do you want to add username?" << endl;
+        cout << "1. Yes\n2. No\nEnter your choice: ";
+        int choice;
+        cin >> choice;
+        if (choice == 1) {
+            int age;
+            string sex;
+            cout << "Enter your age: ";
+            cin >> age;
+            cout << "Enter your sex: ";
+            cin >> sex;
+            ofstream outputFile("input.txt", ios::app);
+            if (!outputFile) {
+                cerr << "Error opening file." << endl;
+                return 1;
+            }
+            outputFile << "\nname : " << name << endl;
+            outputFile << "age : " << age << endl;
+            outputFile << "sex : " << sex << endl;
+            outputFile << endl;
+            outputFile.close();
+            cout << "Name added to the file." << endl;
+        } else {
+            cout << "Exiting without adding name." << endl;
+        }
+        
+    }
+    return 0;
+}
+
+
+
+void checkname(string name, bool &found) {
     ifstream inputFile("input.txt");
     if (!inputFile) {
         cerr << "Error opening file." << endl;
-        return 1;
+        return;
     }
+
     string line;
     while (getline(inputFile, line)) {
-        cout << line << endl;
+        if (line.find("name : " + name) != string::npos) {
+            found = true;
+            cout << "\nFound your name in the file : " << name << endl;
+            cout << "\n------------------------" << endl;
+            cout << line << endl;
+            for(int i = 0; i < 2; i++) {
+                if(getline(inputFile, line)) {
+                    cout << line << endl;
+                }
+            }
+            cout << "------------------------\n" << endl;
+        }
+    }
+    if (!found) {
+        cout << "Name not found in the file." << endl;
     }
     inputFile.close();
-    return 0;
+    return;
 }
