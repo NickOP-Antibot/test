d7158b34b1f2b79c34841ed72301a3b169091bd3
NickOP-Antibot <chitsanupat2549@gmail.com>
2026-02-04 23:36:05 +0700

    Update

--- testing.cpp
+++ testing.cpp
@@ -6,13 +6,21 @@ using namespace std;
 
 void checkname(string &, string &, bool &);
 void addname(string &);
+void fixname(string &);
 
 int main() {
     string name,password;
     bool found = false;
 
-    cout << "Enter your full-name: ";
+    cout << "Enter your full-name : ";
     getline(cin, name);
+    if (name.empty()) {
+        cerr << "Exiting program." << endl;
+        return 0;
+    }
+
+    fixname(name);
+
     checkname(name, password, found);
 
     if (found == false) {
@@ -25,6 +33,7 @@ int main() {
     cin >> cont;
     if (cont == 1) {
         cin.ignore();
+        cout << "------------------------\n" << endl;
         main();
     } else {
         cout << "Exiting program." << endl;
@@ -39,9 +48,9 @@ int main() {
 
 
 void checkname(string &name, string &password, bool &found) {
-    ifstream inputFile("costomer.txt");
+    ifstream inputFile(name + ".txt");
     if (!inputFile) {
-        cerr << "Error opening file." << endl;
+        cerr << "Name not found." << endl;
         return;
     }
 
@@ -52,32 +61,33 @@ void checkname(string &name, string &password, bool &found) {
                 found = true;
                 cout << "Enter your password: ";
                 cin >> password;
-                if (line.find("password : " + password) != string::npos) {
+                if (line.find("password : " + password) != string::npos && line.substr(11) == password) {
                     cout << "Correct password." << endl;
                 } else {
                     cout << "Incorrect password." << endl;
+                    cout << "------------------------\n" << endl;
                     inputFile.close();
                     return;
                 }
             }
-            cout << "\nFound your name in the file : " << name << endl;
-            cout << "\n------------------------" << endl;
+            cout << "\nFound your name : " << name << endl;
+            cout << "\n--------Details---------" << endl;
             for(int i = 0; i < 2; i++) {
                 if(getline(inputFile, line)) {
                     cout << line << endl;
                 }
             }
-            cout << "------------------------\n" << endl;
+            cout << "\n------------------------\n" << endl;
         }
     }
     if (!found) {
-        cout << "Name not found in the file." << endl;
+        cout << "Name not found." << endl;
     }
     inputFile.close();
 }
 
 void addname(string &name) {
-    cout << "\n Do you want to add username?" << endl;
+    cout << "\nDo you want to add username?" << endl;
     cout << "1. Yes\n2. No\nEnter your choice: ";
     int choice;
     cin >> choice;
@@ -91,19 +101,32 @@ void addname(string &name) {
         cin >> sex;
         cout << "Set your password (numeric): ";
         cin >> password;
-        ofstream outputFile("costomer.txt", ios::app);
+        ofstream outputFile(name + ".txt", ios::app);
         if (!outputFile) {
             cerr << "Error opening file." << endl;
             return;
         }
-        outputFile << "\nname : " << name << endl;
+        outputFile << "--------Details---------" << endl;
+        outputFile << "name : " << name << endl;
         outputFile << "password : " << password << endl;
         outputFile << "age : " << age << endl;
         outputFile << "sex : " << sex << endl;
-        outputFile << "------------------------" << endl;
+        outputFile << "\n--------History---------" << endl;
         outputFile.close();
         cout << "Name added to the file." << endl;
     } else {
         cout << "Exiting without adding name." << endl;
+    }
+}
+
+void fixname(string &name) {
+    int N = name.size();
+    for (int i = 0; i < N; i++) {
+        if (name[i] == ' ') {
+            name[i] = '_';
+        }
+        if (name[i] >= 65 && name[i] <= 90) {
+            name[i] = name[i] + 32;
+        }
     }
 }
