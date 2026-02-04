3087781a4ebccc1671b76a4a1a4dae2a39cba905
NickOP-Antibot <chitsanupat2549@gmail.com>
2026-02-04 17:42:13 +0700

    Add initial implementation of file reading in C++

---
+++ testing.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+int main() {
+    ifstream inFile("test.txt");
+    if (!inFile) {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+    string data;
+    while (getline(inFile, data)) {
+        cout << data << endl;
+    }
+    inFile.close();
+    return 0;
+}
